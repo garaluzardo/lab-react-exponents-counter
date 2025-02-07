06b7119e668d0becd915c266f4774092d64f474d
Gara Luzardo <garaluzardo@gmail.com>
2025-02-07 12:31:28 +0000

    Iterations 1, 2, 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import ExponentTwo from "./components/ExponentTwo";
@@ -8,21 +9,28 @@ import ExponentSix from "./components/ExponentSix";
 
 
 function App () {
+// Levantamos el estado aquí:
+const [count, setCount] = useState(1)
+
+// Creamos las funciones para modificar el estado:
+const increment = () => setCount(count + 1);
+const decrement = () => setCount(count - 1);
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} increment={increment} decrement={decrement} />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
       </div>
     </div>
   );
